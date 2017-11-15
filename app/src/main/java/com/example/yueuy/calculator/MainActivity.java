c2d0e6f737e41ee9166fd1dde4e395f80da3347b
yueyy <963723560@qq.com>
2017-11-15 23:43:09 +0800

    Calculator

--- app/src/main/java/com/example/yueuy/calculator/MainActivity.java
+++ app/src/main/java/com/example/yueuy/calculator/MainActivity.java
@@ -2,20 +2,51 @@ package com.example.yueuy.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int[] numbericButton = {R.id.button0,R.id.button1,R.id.button1,R.id.button2,R.id.button3,R.id.button4,R.id.button5,R.id.button6,R.id.button7,R.id.button8,R.id.button9}
+    private int[] numbericButton = {R.id.button0,R.id.button1,R.id.button1,R.id.button2,R.id.button3,R.id.button4,R.id.button5,R.id.button6,R.id.button7,R.id.button8,R.id.button9};
+    private int[] calculateButton = {R.id.buttonDivide,R.id.buttonDot,R.id.buttonMinus,R.id.buttonEquals,R.id.buttonMultiply,R.id.buttonPercent,R.id.buttonPlus};
+    private double valueNumber;
+    private double valueNotNumber = Double.NaN;
+    private boolean delete;
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        for (int i = 0; i < buttons.length; i++) {
-//            temp = (Button) findViewById(button[i])
-//        }
+        this.mEditText = (EditText) findViewById(R.id.editText);
+        setNumericOnClickListener();
+        setCalculateOnClickListener();
+
+    }
+
+    private void setNumericOnClickListener(){
+        View.OnClickListener listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Button button = (Button) v;
+                mEditText.setText(button.getText());
+            }
+        };
+        for (int id : numbericButton) {
+            findViewById(id).setOnClickListener(listener);
+        }
+    }
+
+    private void setCalculateOnClickListener(){
+        View.OnClickListener listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Button button = (Button) v;
+
+            }
+        }
     }
 }
