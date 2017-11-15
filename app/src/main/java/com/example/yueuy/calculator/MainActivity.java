dabac4d8298663361cd22ba1007d025e426ec7a5
yueyy <963723560@qq.com>
2017-11-15 17:36:43 +0800

    Calculator

--- app/src/main/java/com/example/yueuy/calculator/MainActivity.java
+++ app/src/main/java/com/example/yueuy/calculator/MainActivity.java
@@ -8,6 +8,8 @@ import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int[] numbericButton = {R.id.button0,R.id.button1,R.id.button1,R.id.button2,R.id.button3,R.id.button4,R.id.button5,R.id.button6,R.id.button7,R.id.button8,R.id.button9}
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
