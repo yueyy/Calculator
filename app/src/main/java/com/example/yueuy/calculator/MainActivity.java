0462431d4cf95451510e1d0cf960a7fc11ea695b
yueyy <963723560@qq.com>
2017-11-15 09:16:34 +0800

    Calculator

---
+++ app/src/main/java/com/example/yueuy/calculator/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.yueuy.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import static android.R.attr.button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        for (int i = 0; i < buttons.length; i++) {
+//            temp = (Button) findViewById(button[i])
+//        }
+    }
+}
