07dda8f2e1cbd3273fcfc420b504b8e3966b0c71
yueyy <963723560@qq.com>
2017-11-19 02:05:51 +0800

    Calculator

--- app/src/main/java/com/example/yueuy/calculator/MainActivity.java
+++ app/src/main/java/com/example/yueuy/calculator/MainActivity.java
@@ -6,16 +6,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import static android.R.attr.button;
+import javax.xml.xpath.XPathExpression;
 
 public class MainActivity extends AppCompatActivity {
 
     private int[] numbericButton = {R.id.button0,R.id.button1,R.id.button1,R.id.button2,R.id.button3,R.id.button4,R.id.button5,R.id.button6,R.id.button7,R.id.button8,R.id.button9};
-    private int[] calculateButton = {R.id.buttonDivide,R.id.buttonDot,R.id.buttonMinus,R.id.buttonEquals,R.id.buttonMultiply,R.id.buttonPercent,R.id.buttonPlus};
+    private int[] calculateButton = {R.id.buttonDivide,R.id.buttonDot,R.id.buttonMinus,R.id.buttonEqual,R.id.buttonMultiply,R.id.buttonPercent,R.id.buttonPlus};
     private double valueNumber;
-    private double valueNotNumber = Double.NaN;
-    private boolean delete;
+//    private boolean delete;
     private EditText mEditText;
+    private String operation = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +45,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 Button button = (Button) v;
-
+                mEditText.setText(button.getText());
             }
+        };
+        for(int id: calculateButton){
+            findViewById(id).setOnClickListener(listener);
         }
+
+        findViewById(R.id.buttonDelete).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mEditText.setText("");
+            }
+        });
+
+        findViewById(R.id.buttonEqual).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                onEqual();
+            }
+        });
+    }
+
+    private void onEqual(){
+        String txt = mEditText.getText().toString();
+    }
+
+    public void onClick(View v){
+        float num1 = 0;
+        float num2 = 0;
+        float result = 0;
+        num1 = Float.parseFloat(mEditText.getText().toString());
+        switch (v.getId())
+        case R.id.buttonMultiply:
+            operation = "+";
+        result = num1 + num2;
+
+
     }
 }
